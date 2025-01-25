08b32745339dc56bc00317c5bb2832b799656875
Chinnaphat Lohasangsuwan <110705914+ChinnaphatLoha@users.noreply.github.com>
2025-01-26 02:02:42 +0700

    add Discord bot and update env (#2)

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-import { configDotenv } from "dotenv";
-import OpenAI from "openai";
+import { configDotenv } from 'dotenv';
+import OpenAI from 'openai';
+import { Client } from 'discord.js';
 
 configDotenv();
 
+const client = new Client({
+	intents: ['Guilds', 'GuildMessages', 'GuildMembers', 'MessageContent'],
+});
+
+const CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
+
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.OPENAI_API_KEY,
 });
 
-const completion = openai.chat.completions.create({
-  model: "gpt-4o-mini",
-  store: true,
-  messages: [
-    {"role": "user", "content": "write a haiku about ai"},
-  ],
+client.on('messageCreate', async (message) => {
+	if (message.author.bot) return;
+	if (message.channel.id !== CHANNEL_ID) return;
+
+	console.log(`Hello ${message.content}!`);
 });
 
-completion.then((result) => console.log(result.choices[0].message));
+client.login(process.env.DISCORD_BOT_TOKEN);
